86ff94ac87e6f19ff7c5da888417b62d69b8f8b9
jayaprasad356 <jayaprasad356@gmail.com>
2022-06-05 12:16:27 +0530

    update_profile

--- api/update_profile.php
+++ api/update_profile.php
@@ -28,15 +28,36 @@ if (empty($_POST['email'])) {
     print_r(json_encode($response));
     return false;
 }
+if (empty($_POST['weight'])) {
+    $response['success'] = false;
+    $response['message'] = "Weight is Empty";
+    print_r(json_encode($response));
+    return false;
+}
+if (empty($_POST['height'])) {
+    $response['success'] = false;
+    $response['message'] = "Height is Empty";
+    print_r(json_encode($response));
+    return false;
+}
+if (empty($_POST['age'])) {
+    $response['success'] = false;
+    $response['message'] = "Age is Empty";
+    print_r(json_encode($response));
+    return false;
+}
 $user_id = $db->escapeString($_POST['user_id']);
 $name = (isset($_POST['name']) && !empty(trim($_POST['name']))) ? $db->escapeString(trim($_POST['name'])) : '';
 $email = (isset($_POST['email']) && !empty(trim($_POST['email']))) ? $db->escapeString(trim($_POST['email'])) : '';
+$weight = (isset($_POST['weight']) && !empty(trim($_POST['weight']))) ? $db->escapeString(trim($_POST['weight'])) : '';
+$height = (isset($_POST['height']) && !empty(trim($_POST['height']))) ? $db->escapeString(trim($_POST['height'])) : '';
+$age = (isset($_POST['age']) && !empty(trim($_POST['age']))) ? $db->escapeString(trim($_POST['age'])) : '';
 $sql = "SELECT * FROM users WHERE id = '" . $user_id . "'";
 $db->sql($sql);
 $res = $db->getResult();
 $num = $db->numRows($res);
 if ($num == 1) {
-    $sql = "UPDATE `users` SET `name`='$name',`email`='$email' WHERE id=" . $user_id;
+    $sql = "UPDATE `users` SET `name`='$name',`email`='$email',`weight`='$weight',`height`='$height',`age`='$age' WHERE id=" . $user_id;
     $db->sql($sql);
     $sql = "SELECT * FROM users WHERE id = '" . $user_id . "'";
     $db->sql($sql);

40719e08c370db90d4e6d85aec854fdce76a307a
Divaharsha <divakarvan03@gmail.com>
2022-06-09 14:14:38 -0700

    admin panel updated

---
+++ api/get-bootstrap-table-data.php
@@ -0,0 +1,119 @@
+<?php
+session_start();
+
+// set time for session timeout
+$currentTime = time() + 25200;
+$expired = 3600;
+
+// // if session not set go to login page
+// if (!isset($_SESSION['user'])) {
+//     header("location:index.php");
+// }
+
+// // if current time is more than session timeout back to login page
+// if ($currentTime > $_SESSION['timeout']) {
+//     session_destroy();
+//     header("location:index.php");
+// }
+
+// destroy previous session timeout and create new one
+unset($_SESSION['timeout']);
+$_SESSION['timeout'] = $currentTime + $expired;
+
+header('Access-Control-Allow-Origin: *');
+header("Content-Type: application/json");
+header("Expires: 0");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+
+
+include_once('../includes/crud.php');
+
+$db = new Database();
+$db->connect();
+
+
+
+
+
+
+if (isset($_GET['table']) && $_GET['table'] == 'users') 
+{
+    $offset = 0;
+    $limit = 10;
+    $where = '';
+    $sort = 'id';
+    $order = 'DESC';
+    if (isset($_GET['offset']))
+        $offset = $db->escapeString($_GET['offset']);
+    if (isset($_GET['limit']))
+        $limit = $db->escapeString($_GET['limit']);
+    if (isset($_GET['sort']))
+        $sort = $db->escapeString($_GET['sort']);
+    if (isset($_GET['order']))
+        $order = $db->escapeString($_GET['order']);
+
+    if (isset($_GET['search']) && !empty($_GET['search'])) {
+        $search = $db->escapeString($_GET['search']);
+        $where .= "WHERE name like '%" . $search . "%' OR id like '%" . $search . "%' OR email like '%" . $search . "%' OR mobile like '%" . $search . "%' ";
+    }
+    if (isset($_GET['sort'])){
+        $sort = $db->escapeString($_GET['sort']);
+
+    }
+    if (isset($_GET['order'])){
+        $order = $db->escapeString($_GET['order']);
+
+    }
+    $sql = "SELECT COUNT(`id`) as total FROM `users` " . $where;
+    $db->sql($sql);
+    $res = $db->getResult();
+   
+    
+    foreach ($res as $row)
+        $total = $row['total'];
+       
+   
+    //$sql = "SELECT * FROM users $where ORDER BY $sort $order";
+    $sql = "SELECT * FROM users " . $where . " ORDER BY " . $sort . " " . $order . " LIMIT " . $offset . ", " . $limit;
+    $db->sql($sql);
+    $res = $db->getResult();
+    
+    $bulkData = array();
+    $bulkData['total'] = $total;
+   
+    
+    $rows = array();
+    $tempRow = array();
+
+   
+    
+    foreach ($res as $row) {
+        $sql="SELECT * FROM users";
+        $db->sql($sql);
+        $res = $db->getResult();
+
+    
+        $tempRow['id'] = $row['id'];
+        $tempRow['name'] = $row['name'];
+        $tempRow['email'] = $row['email'];
+        $tempRow['gender'] = $row['gender'];
+        $tempRow['weight'] = $row['weight'];
+        $tempRow['height'] = $row['height'];
+        $tempRow['age'] = $row['age'];
+        $operate= '<a href="edit-users.php?id=' . $row['id'] . '"><i class="fa fa-edit"></i></a>';
+        $tempRow['operate'] = $operate;
+        
+        
+        $rows[] = $tempRow;
+    }
+    $bulkData['rows'] = $rows;
+    print_r(json_encode($bulkData));
+}
+
+
+
+$db->disconnect();
---
+++ crypto.sql
@@ -0,0 +1,148 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 09, 2022 at 11:08 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 7.4.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `crypto`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reward_settings`
+--
+
+CREATE TABLE `reward_settings` (
+  `id` int(11) NOT NULL,
+  `value` text DEFAULT NULL,
+  `reward` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `reward_settings`
+--
+
+INSERT INTO `reward_settings` (`id`, `value`, `reward`) VALUES
+(1, '2000', '100'),
+(2, '4000', '90'),
+(3, '6000', '80'),
+(4, '8000', '70'),
+(5, '10000', '60'),
+(6, '12000', '50'),
+(7, '14000', '40'),
+(8, '16000', '30'),
+(9, '18000', '20'),
+(10, '20000', '10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `steps`
+--
+
+CREATE TABLE `steps` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `date` text DEFAULT NULL,
+  `steps` int(11) DEFAULT NULL,
+  `calories` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `steps`
+--
+
+INSERT INTO `steps` (`id`, `user_id`, `date`, `steps`, `calories`) VALUES
+(2, 1, '2022-06-06', 5, 45),
+(3, 1, '2022-06-08', 50, 450);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` text DEFAULT NULL,
+  `email` text DEFAULT NULL,
+  `password` text DEFAULT NULL,
+  `gender` text DEFAULT NULL,
+  `weight` int(11) DEFAULT NULL,
+  `height` int(11) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `gender`, `weight`, `height`, `age`) VALUES
+(1, 'Jaya Prasad', 'jp@gmail.com', '12345678', NULL, 45, 34, 45),
+(2, 'Prasad', 'jp1@gmail.com', '12345678', 'male', NULL, NULL, NULL),
+(3, 'Vijay', 'vjdeveloper2020@gmail.com', '123456', 'Male', 48, 160, 20),
+(4, 'Prasad', 'prasad@gmail.com', '12345678', 'Male', 50, 165, 20);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `reward_settings`
+--
+ALTER TABLE `reward_settings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `steps`
+--
+ALTER TABLE `steps`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `reward_settings`
+--
+ALTER TABLE `reward_settings`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `steps`
+--
+ALTER TABLE `steps`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

16f4d224b6424f0720976098c3b99c9b524fb8e7
jayaprasad356 <jayaprasad356@gmail.com>
2022-06-30 19:46:39 +0530

    signup

--- api/signup.php
+++ api/signup.php
@@ -7,8 +7,6 @@ header("Cache-Control: no-store, no-cache, must-revalidate");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 date_default_timezone_set('Asia/Kolkata');
-echo date('d-m-Y');
-
 include_once('../includes/crud.php');
 $db = new Database();
 $db->connect();
